b85148f6b7d127c531b5ab4a94284c0edd20b001
htheo <hinfraytheo@gmail.com>
2016-04-16 15:24:10 +0200

    edit de trajet avec problème de vitesse

--- js/function.js
+++ js/function.js
@@ -1,8 +1,27 @@
+/*var bateaux = []; //tableau de tous les bateaux
 //edit de bateaux
 function editbateau(bat){
+    addEventListener('click', function(){
     console.log("bien joué"+bat);
-     map.on('click', addLatLngToPolyline); //Listen for clicks on map.
+    $('#stopEditPolyline').css('display','block');
+   
+    bateau[bat]=moveTo([3,3], 20000);
+    console.log(bateau[bat]);
+    //currentPolyline = new L.polyline([]).addTo(map);
+    //map.on('click', editLatLngToPolyline); //Listen for clicks on map.*/
+    /*
+});
+    
 }
 
-
+function editLatLngToPolyline(clickEventData){
+        //console.log(clickEventData);
+        //console.log(clickEventData.latlng.lat)
+        currentPolyline.addLatLng(clickEventData.latlng);
+    
+}
+stopEditTrajet.addEventListener('click', function(){
+    map.off('click', editLatLngToPolyline); //Stop listening for clicks on map.
+});
+*/
 
