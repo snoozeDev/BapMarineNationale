fcea060084c0a54d9fcef375988c1772f3576327
Simon Antonelli <santonel.iim@gmail.com>
2016-05-19 11:16:00 +0200

    Onglets sauf création de forme car trop de conflits (A régler)

--- map.php
+++ map.php
@@ -10,6 +10,17 @@
 
   <link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css' rel='stylesheet' />
 
+  <script type="text/javascript">
+    function change_onglet(name)
+    {
+      document.getElementById('onglet_'+anc_onglet).className = 'onglet_0 onglet';
+      document.getElementById('onglet_'+name).className = 'onglet_1 onglet';
+      document.getElementById('contenu_onglet_'+anc_onglet).style.display = 'none';
+      document.getElementById('contenu_onglet_'+name).style.display = 'block';
+      anc_onglet = name;
+    }
+  </script>
+
 
 </head>
 
@@ -24,7 +35,7 @@
       <a class="btn" id="btn_circle">Cercle</a>  
       <a class="btn" id="btn_polygone">Polygone</a>
       <a class="btn" id="btn_polyline">Lignes</a></div>
-    </br></br>
+      <div class="ligne">
     <div id="form_text" class="formulaire hide">
       <p class="title"> Ecrire sur la carte </p>
       <input id="label_text" class="formula" placeholder="Votre texte"><br><br>
@@ -33,6 +44,7 @@
       <a href="#" class="btn"  id="drawText">Ajouter texte</a>
 
     </div>
+    
     <div id="form_circle" class="formulaire hide">
      <p class="title">Créer un cercle</p>
 
@@ -78,6 +90,8 @@
 
 </div>
 
+</div>
+
 
 
 
@@ -95,7 +109,7 @@
     <option value="avion">Avion</option>
   </select>
   <label>Vitesse du trajet (en noeud)</label><br>
-   <input type="text" id="vitesse_bateau" value="189.525"><br><br>
+  <input type="text" id="vitesse_bateau" value="189.525"><br><br>
   
   <label>Alignement</label><br>
   <select class="formula" id="color_bateau" name="color_bateau">
@@ -103,7 +117,7 @@
     <option value="green">Neutre</option>
     <option value="red">Ennemi</option>
   </select><br>
- 
+
   <label>Description (facutatif)</label><br>
   <textarea class="formula" id="description" name="description"></textarea>
   <a href="#" class="btn" id="drawTrajet">Ajouter Trajet</a>
@@ -118,7 +132,7 @@
      <img src="image/pause.png" style="display:none"  id="pause" class="btntime">
    </div>
    
-    <div id="timer">
+   <div id="timer">
 
 
      <span id="days">00J</span><br> 
@@ -127,7 +141,7 @@
 
    </div>
 
-  <div class="timer_number_titles">
+   <div class="timer_number_titles">
 <!--
               <div class="timer_number_days">
                 Jours
@@ -138,139 +152,153 @@
               <div class="timer_number_mins">
                 Minutes
               </div>
--->
-  </div>
+            -->
+          </div>
 
 
-   <div>
-    <select class=" speed">
+          <div>
+            <select class=" speed">
 
-      <option value="1">x1</option>
-      <option value="3">x3</option>
-      <option value="5">x5</option>
-      <option value="10">x10</option>
-      <option value="50">x50</option>
-      <option value="100">100</option>
-
-
-
-    </select></div></div></div></div>
-
-  <img id="boussole" src="image/boussole.png">
-  <div id="map">
+              <option value="1">x1</option>
+              <option value="3">x3</option>
+              <option value="5">x5</option>
+              <option value="10">x10</option>
+              <option value="50">x50</option>
+              <option value="100">100</option>
 
 
 
-  </div>
-  <div id="toolbar">
-    <div class="forme" id="bateau"> <img src="image/bateau.png"></div>
+            </select></div></div></div></div>
 
-    <div class="forme" id="forme"> <img src="image/forme.png"></div>
-  </div>
-
-  <div class="stop">
-    <input class="hide" id="editVitesse" placeholder="Votre Vitesse"><br>
-
-    <div class="hide btn_finish forme" id="stopDraw"> <img src="image/formev2.png"></div>
-    <div class="hide btn_finish forme" id="stopDrawPolyline"> <img src="image/formev2.png"></div>
-    <div class="hide btn_finish forme" id="stopEditPolyline"> <img src="image/bateauv2.png"></div>
-    <div class="hide btn_finish forme" id="stopDrawTrajet"> <img src="image/bateauv2.png"></div>
+            <img id="boussole" src="image/boussole.png">
+            <div id="map">
 
 
-  </div>
+
+            </div>
+            <div id="toolbar">
+              <div class="forme" id="bateau"> <img src="image/bateau.png"></div>
+
+              <div class="forme" id="forme"> <img src="image/forme.png"></div>
+            </div>
+
+            <div class="stop">
+              <input class="hide" id="editVitesse" placeholder="Votre Vitesse"><br>
+
+              <div class="hide btn_finish forme" id="stopDraw"> <img src="image/formev2.png"></div>
+              <div class="hide btn_finish forme" id="stopDrawPolyline"> <img src="image/formev2.png"></div>
+              <div class="hide btn_finish forme" id="stopEditPolyline"> <img src="image/bateauv2.png"></div>
+              <div class="hide btn_finish forme" id="stopDrawTrajet"> <img src="image/bateauv2.png"></div>
 
 
-          <button id="sidebar-btn" class="sidebar-btn">
-          <img id="menu-stripes1" src="image/menu.png">
-          </button>
-          <section id="sidebar" class="sidebar">
-            <div class="settings">
-              <h1>Parametres</h1>
+            </div>
 
-              <button id="onglet-btn" class="onglet1-btn onglet-btn">
-             <h2>Filtres</h2>
-              </button>
-              <button id="onglet-btn" class="onglet2-btn onglet-btn">
+
+            <button id="sidebar-btn" class="sidebar-btn">
+              <img id="menu-stripes1" src="image/menu.png">
+            </button>
+            <section id="sidebar" class="sidebar">
+              <div class="settings">
+                <h1>Parametres</h1>
+
+                <div class="onglets">
+                  <span class="onglet_0 onglet" id="onglet_filtres" onclick="javascript:change_onglet('filtres');">Filtres</span>
+                  <span class="onglet_0 onglet" id="onglet_suppression" onclick="javascript:change_onglet('suppression');">Suppression</span>
+                </div>
+
+                <!-- <button id="onglet-btn" class="onglet1-btn onglet-btn active">
+                 <h2>Filtres</h2>
+               </button>
+               <button id="onglet-btn" class="onglet2-btn onglet-btn inactive">
                 <h2>Suppression</h2>
-              </button>
+              </button> -->
 
-              <div class="onglet1 is-open">
-<div class="filtres-cont">
-                <div style="padding-top:30px" class="bouton">
-                  <input class="checkbox" id="checkbox1"  type="checkbox">
-                  <label id="profondeur" for="checkbox1"></label>
-                  <span class="filtres">Cacher les profondeurs</span>
-                </div><br>
-                <div class="bouton">
-                  <input class="checkbox" id="checkbox2"  type="checkbox">
-                  <label  id="riv" for="checkbox2"></label>
-                  <span class="filtres">Cacher les côtes et les rivières</span>
-                </div><br>
-                <div class="bouton">
-                  <input class="checkbox" id="checkbox3" type="checkbox">
-                  <label  id="structures" for="checkbox3"></label>
-                  <span class="filtres">Cacher les structures</span>
-                </div><br>
-                  </div>
+              <!-- <div class="onglet1 is-open"> -->
+              <div class="contenu_onglet" id="contenu_onglet_filtres">
+                <div class="filtres-cont">
+                  <div style="padding-top:30px" class="bouton">
+                    <input class="checkbox" id="checkbox1"  type="checkbox">
+                    <label id="profondeur" for="checkbox1"></label>
+                    <span class="filtres">Cacher les profondeurs</span>
+                  </div><br>
+                  <div class="bouton">
+                    <input class="checkbox" id="checkbox2"  type="checkbox">
+                    <label  id="riv" for="checkbox2"></label>
+                    <span class="filtres">Cacher les côtes et les rivières</span>
+                  </div><br>
+                  <div class="bouton">
+                    <input class="checkbox" id="checkbox3" type="checkbox">
+                    <label  id="structures" for="checkbox3"></label>
+                    <span class="filtres">Cacher les structures</span>
+                  </div><br>
+                </div>
               </div>
 
 
-              <div class="onglet2 is-closed" style="background-color: #2C3E50;">
+              <!-- <div class="onglet2 is-closed" style="background-color: #2C3E50;"> -->
+              <div class="contenu_onglet" id="contenu_onglet_suppression">
 
-                  <div class="delete" style="position: relative;top:100px;">
-
-
-                      <div class="delete_polyline" style="color: #82C9BD;">
-                          <div class="share-wrap">
-                              <div class="main-bar1 main-bar" style="color: #82C9BD;">Ligne<span><img src="http://static.tumblr.com/43vgkmu/ftqmmywe6/drop-down.png"/></span></div>
-
-                              <ul class="lignes">
-
-                                  <br/>
-                                  <li>  <p class="delete_polyline_p"></p> </li>
-                              </ul>
-                          </div>
+              <div class="delete" style="position: relative;">
 
 
-                          <div class="delete_cercle">
-                              <div class="share-wrap">
-                                  <div class="main-bar2 main-bar" style="color: #82C9BD;">cercle<span><img src="http://static.tumblr.com/43vgkmu/ftqmmywe6/drop-down.png"/></span></div>
-                                  <ul class="cercle">
-                                      <br/>
-                                      <li> <p class="delete_cercle_p"></p></li>
-                                  </ul>
-                              </div>
-                          </div>
+                  <div class="delete_polyline" style="color: #82C9BD;">
+                    <div class="share-wrap">
+                      <div class="main-bar1 main-bar" style="color: #82C9BD;">Ligne<span><img src="http://static.tumblr.com/43vgkmu/ftqmmywe6/drop-down.png"/></span></div>
+
+                      <ul class="lignes">
+
+                        <br/>
+                        <li>  <p class="delete_polyline_p"></p> </li>
+                      </ul>
+                    </div>
 
 
-                          <div class="delete_texte">
-                              <div class="share-wrap">
-                                  <div class="main-bar3 main-bar" style="color: #82C9BD;">texte<span><img src="http://static.tumblr.com/43vgkmu/ftqmmywe6/drop-down.png"/></span></div>
-                                  <ul class="texte">
-                                      <br/>
-                                      <li> <p class="delete_texte_p"></p></li>
-                                  </ul>
-                              </div>
-                          </div>
-
-
-                          <div class="delete_polygone">
-                              <div class="share-wrap">
-                                  <div class="main-bar4 main-bar" style="color: #82C9BD;">polygone<span><img src="http://static.tumblr.com/43vgkmu/ftqmmywe6/drop-down.png"/></span></div>
-                                  <ul class="polygone">
-                                      <br/>
-                                      <li>  <p class="delete_polygone_p"></p></li>
-                                  </ul>
-                              </div>
-                          </div>
-
+                    <div class="delete_cercle">
+                      <div class="share-wrap">
+                        <div class="main-bar2 main-bar" style="color: #82C9BD;">cercle<span><img src="http://static.tumblr.com/43vgkmu/ftqmmywe6/drop-down.png"/></span></div>
+                        <ul class="cercle">
+                          <br/>
+                          <li> <p class="delete_cercle_p"></p></li>
+                        </ul>
                       </div>
+                    </div>
+
+
+                    <div class="delete_texte">
+                      <div class="share-wrap">
+                        <div class="main-bar3 main-bar" style="color: #82C9BD;">texte<span><img src="http://static.tumblr.com/43vgkmu/ftqmmywe6/drop-down.png"/></span></div>
+                        <ul class="texte">
+                          <br/>
+                          <li> <p class="delete_texte_p"></p></li>
+                        </ul>
+                      </div>
+                    </div>
+
+
+                    <div class="delete_polygone">
+                      <div class="share-wrap">
+                        <div class="main-bar4 main-bar" style="color: #82C9BD;">polygone<span><img src="http://static.tumblr.com/43vgkmu/ftqmmywe6/drop-down.png"/></span></div>
+                        <ul class="polygone">
+                          <br/>
+                          <li>  <p class="delete_polygone_p"></p></li>
+                        </ul>
+                      </div>
+                    </div>
 
                   </div>
 
+                </div>
+
+              </div>
+
+              <script type="text/javascript">
+                var anc_onglet = 'filtres';
+                change_onglet(anc_onglet);
+              </script>
 
 
-          </section>
+
+            </section>
 
             <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
             <script src="https://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
@@ -333,14 +361,14 @@
                   $toggleCol.toggleClass('sidebar-show');
                   $sidebar.toggleClass('show');
 
-                        var image = document.getElementById('menu-stripes1');
+                  var image = document.getElementById('menu-stripes1');
 
                   if (!sidebarIsVisible) {
-                      
-                      image.src = "image/cancel.png"; 
+
+                    image.src = "image/cancel.png"; 
                     sidebarIsVisible = true;
                   } else {
-                        image.src = "image/menu.png"; 
+                    image.src = "image/menu.png"; 
                     sidebarIsVisible = false;
                   }
                 }
@@ -366,35 +394,35 @@
 
             </script>
 
-  <script>
-      $(document).ready(function(e){
-          $('.main-bar1').on('click',function(){
-              $('ul.lignes').slideToggle(1000);
-          });
-      })
-  </script>
-  <script>
-      $(document).ready(function(e){
-          $('.main-bar2').on('click',function(){
-              $('ul.cercle').slideToggle(1580);
-          });
-      })
-  </script>
-  <script>
-      $(document).ready(function(e){
-          $('.main-bar3').on('click',function(){
-              $('ul.texte').slideToggle(1580);
-          });
-      })
-  </script>
-  <script>
-      $(document).ready(function(e){
-          $('.main-bar4').on('click',function(){
-              $('ul.polygone').slideToggle(1580);
-          });
-      })
-  </script>
-           
+            <script>
+              $(document).ready(function(e){
+                $('.main-bar1').on('click',function(){
+                  $('ul.lignes').slideToggle(1000);
+                });
+              })
+            </script>
+            <script>
+              $(document).ready(function(e){
+                $('.main-bar2').on('click',function(){
+                  $('ul.cercle').slideToggle(1580);
+                });
+              })
+            </script>
+            <script>
+              $(document).ready(function(e){
+                $('.main-bar3').on('click',function(){
+                  $('ul.texte').slideToggle(1580);
+                });
+              })
+            </script>
+            <script>
+              $(document).ready(function(e){
+                $('.main-bar4').on('click',function(){
+                  $('ul.polygone').slideToggle(1580);
+                });
+              })
+            </script>
+
 
           </body>
           </html>
