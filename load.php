65e83c85d3236c674874134e8586aea071cdd3d2
momolafripouille <Nicolas-g7@hotmail.fr>
2016-05-28 16:46:46 +0200

    load.php -> mysqli

--- load.php
+++ load.php
@@ -8,53 +8,59 @@ function phpAlert($msg) {
 include 'db.php';   
  
      
-    $connect = mysql_connect($dbhost,$dbuser,$dbpass);
-                mysql_select_db($db);
-                mysql_query("SET NAMES UTF8"); 
-                $query = mysql_query("SELECT * FROM save_marine_table WHERE ID = $_GET[id]");
-                while($rows = mysql_fetch_array($query)):
-                $id = $rows['id'];
-                $mapName = $rows['mapName'];
-                $cercles = $rows['cercles']; 
-    $polygs = $rows['polygs']; 
-      $texts = $rows['texts'];
-    $polyls = $rows['polyls'];
-    $batis = $rows['batis'];
-    $bateaux = $rows['bateaux'];
+
     
-    if(strlen($cercles) !== 0){
-        echo "
-        <script> var cerclesPhp = $cercles;loadCercle(cerclesPhp);</script>
-        ";
-    }
-      if(strlen($polygs) !== 0){
-        echo "
-        <script> var polygsPhp = $polygs;loadPolyg(polygsPhp);</script>
-        ";
-    }
-      if(strlen($texts) !== 0){
-        echo "
-        <script> var textsPhp = $texts;loadText(textsPhp);</script>
-        ";
-    }
-     if(strlen($polyls) !== 0){
-        echo "
-        <script> var polylsPhp = $polyls;loadPolyl(polylsPhp);</script>
-        ";
-    }
-    if(strlen($batis) !== 0){
-        echo "
-        <script> var batisPhp = $batis;loadBati(batisPhp);</script>
-        ";
-    }
-    if(strlen($bateaux) !== 0){
-        echo "
-        <script> var bateauxPhp = $bateaux;loadBateaux(bateauxPhp);</script>
-        ";
-    }
-           endwhile;
-                 
     
+    
+    $conn = new mysqli($dbhost, $dbuser, $dbpass, $db);
+    
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT id, mapName, mapDescription, cercles, polygs, texts, polyls, batis, bateaux FROM save_marine_table";
+$result = $conn->query($sql);
+
+    
+    
+    while($row = $result->fetch_assoc()) {
+    
+      if($row["id"] == $_GET[id]){  
+        
+       
+        
+    if(strlen($row["cercles"]) !== 0){
+        echo "
+        <script> var cerclesPhp = ".$row["cercles"].";loadCercle(cerclesPhp);</script>
+        ";
+    }
+      if(strlen($row["polygs"]) !== 0){
+        echo "
+        <script> var polygsPhp = ".$row["polygs"].";loadPolyg(polygsPhp);</script>
+        ";
+    }
+      if(strlen($row["texts"]) !== 0){
+        echo "
+        <script> var textsPhp = ".$row["texts"].";loadText(textsPhp);</script>
+        ";
+    }
+     if(strlen($row["polyls"]) !== 0){
+        echo "
+        <script> var polylsPhp = ".$row["polyls"].";loadPolyl(polylsPhp);</script>
+        ";
+    }
+    if(strlen($row["batis"]) !== 0){
+        echo "
+        <script> var batisPhp = ".$row["batis"].";loadBati(batisPhp);</script>
+        ";
+    }
+    if(strlen($row["bateaux"]) !== 0){
+        echo "
+        <script> var bateauxPhp = ".$row["bateaux"].";loadBateaux(bateauxPhp);</script>
+        ";
+    }
+     
+    }
+    }
 }
 
 ?>
