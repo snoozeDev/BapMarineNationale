c42856e23585e13e1dfabb314116c38ba425b10a
momolafripouille <Nicolas-g7@hotmail.fr>
2016-05-31 15:38:37 +0200

    clean code|suppression js dans le php

--- js/function.js
+++ js/function.js
@@ -24,6 +24,105 @@
  map.off('click', editLatLngToPolyline); //Stop listening for clicks on map.
  });
  */
+
+
+
+        var sidebar = (function () {
+            "use strict";
+
+            var $contnet = $('#content')
+                , $sidebar = $('#sidebar')
+                , $sidebarBtn = $('#sidebar-btn')
+                , $toggleCol = $('body').add($contnet).add($sidebarBtn)
+                , sidebarIsVisible = false;
+
+            $sidebarBtn.on('click', function () {
+
+                if (!sidebarIsVisible) {
+                    bindContent();
+                } else {
+                    unbindContent();
+                }
+
+                toggleMenu();
+            });
+
+
+            function bindContent() {
+
+                $contnet.on('click', function () {
+                    toggleMenu();
+                    unbindContent();
+                });
+            }
+
+            function unbindContent() {
+                $contnet.unbind();
+            }
+
+            function toggleMenu() {
+
+                $toggleCol.toggleClass('sidebar-show');
+                $sidebar.toggleClass('show');
+
+                var image = document.getElementById('menu-stripes1');
+
+                if (!sidebarIsVisible) {
+
+                    image.src = "image/cancel.png";
+                    sidebarIsVisible = true;
+                } else {
+                    image.src = "image/menu.png";
+                    sidebarIsVisible = false;
+                }
+            }
+
+
+            var $menuToggle = $sidebar.find('.menu-toggle');
+
+            $menuToggle.each(function () {
+
+                var $this = $(this)
+                    , $submenuBtn = $this.children('.menu-toggle-btns').find('.menu-btn')
+                    , $submenu = $this.children('.submenu');
+
+                $submenuBtn.on('click', function (e) {
+                    e.preventDefault();
+                    $submenu.slideToggle();
+                    $(this).toggleClass('active');
+                });
+            });
+
+        })();
+
+        $(document).ready(function (e) {
+            $('.main-bar1').on('click', function () {
+                $('ul.lignes').slideToggle(120);
+            });
+        })
+        $(document).ready(function (e) {
+            $('.main-bar5').on('click', function () {
+                $('ul.batiments').slideToggle(120);
+            });
+        })
+        $(document).ready(function (e) {
+            $('.main-bar2').on('click', function () {
+                $('ul.cercle').slideToggle(120);
+            });
+        })
+        $(document).ready(function (e) {
+            $('.main-bar3').on('click', function () {
+                $('ul.texte').slideToggle(120);
+            });
+        })
+        $(document).ready(function (e) {
+            $('.main-bar4').on('click', function () {
+                $('ul.polygone').slideToggle(120);
+            });
+        })
+
+
+
 var doubleclique=0;
 function delete_obj(elem, form){
     if($('.'+elem).css('display') == 'none'){
--- map.php
+++ map.php
@@ -460,7 +460,7 @@ $result = $conn->query($sql);
 
     <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
 
-    <script src="js/Leaflet-0.7.7/leaflet.js"></script>
+    <script src="js/leaflet.js"></script>
     <script type="text/javascript" src="js/icone.js"></script>
     <script type="text/javascript" src="js/map.js" charset="UTF-8"></script>
     <script src='js/coord.js'></script>
@@ -472,103 +472,13 @@ $result = $conn->query($sql);
 
 
 
+<!--
     <script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js'></script>
     <link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css' rel='stylesheet' />
+-->
 
     <script>
-        var sidebar = (function () {
-            "use strict";
 
-            var $contnet = $('#content')
-                , $sidebar = $('#sidebar')
-                , $sidebarBtn = $('#sidebar-btn')
-                , $toggleCol = $('body').add($contnet).add($sidebarBtn)
-                , sidebarIsVisible = false;
-
-            $sidebarBtn.on('click', function () {
-
-                if (!sidebarIsVisible) {
-                    bindContent();
-                } else {
-                    unbindContent();
-                }
-
-                toggleMenu();
-            });
-
-
-            function bindContent() {
-
-                $contnet.on('click', function () {
-                    toggleMenu();
-                    unbindContent();
-                });
-            }
-
-            function unbindContent() {
-                $contnet.unbind();
-            }
-
-            function toggleMenu() {
-
-                $toggleCol.toggleClass('sidebar-show');
-                $sidebar.toggleClass('show');
-
-                var image = document.getElementById('menu-stripes1');
-
-                if (!sidebarIsVisible) {
-
-                    image.src = "image/cancel.png";
-                    sidebarIsVisible = true;
-                } else {
-                    image.src = "image/menu.png";
-                    sidebarIsVisible = false;
-                }
-            }
-
-
-            var $menuToggle = $sidebar.find('.menu-toggle');
-
-            $menuToggle.each(function () {
-
-                var $this = $(this)
-                    , $submenuBtn = $this.children('.menu-toggle-btns').find('.menu-btn')
-                    , $submenu = $this.children('.submenu');
-
-                $submenuBtn.on('click', function (e) {
-                    e.preventDefault();
-                    $submenu.slideToggle();
-                    $(this).toggleClass('active');
-                });
-            });
-
-        })();
-
-        $(document).ready(function (e) {
-            $('.main-bar1').on('click', function () {
-                $('ul.lignes').slideToggle(120);
-            });
-        })
-        $(document).ready(function (e) {
-            $('.main-bar5').on('click', function () {
-                $('ul.batiments').slideToggle(120);
-            });
-        })
-        $(document).ready(function (e) {
-            $('.main-bar2').on('click', function () {
-                $('ul.cercle').slideToggle(120);
-            });
-        })
-        $(document).ready(function (e) {
-            $('.main-bar3').on('click', function () {
-                $('ul.texte').slideToggle(120);
-            });
-        })
-        $(document).ready(function (e) {
-            $('.main-bar4').on('click', function () {
-                $('ul.polygone').slideToggle(120);
-            });
-        })
     </script>
 
 
